088bb31d201c57ae3bba24aa293637ffee5ab4d8
smaximov <s.b.maximov@gmail.com>
2016-05-13 23:47:57 +1000

    Colors

--- tools/install.sh
+++ tools/install.sh
@@ -13,23 +13,26 @@ ZSH_CACHE_DIR=$XDG_CACHE_HOME/zsh
 OH_MY_ZSH_REPO="https://github.com/robbyrussell/oh-my-zsh.git"
 ZSH_CONFIG_REPO="https://github.com/smaximov/zsh-config.git"
 
+if which tput >/dev/null 2>&1; then
+    ncolors=$(tput colors)
+fi
+if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
+    RED="$(tput setaf 1)"
+    GREEN="$(tput setaf 2)"
+    YELLOW="$(tput setaf 3)"
+    BLUE="$(tput setaf 4)"
+    BOLD="$(tput bold)"
+    NORMAL="$(tput sgr0)"
+else
+    RED=""
+    GREEN=""
+    YELLOW=""
+    BLUE=""
+    BOLD=""
+    NORMAL=""
+fi
+
 main() {
-    if which tput >/dev/null 2>&1; then
-        ncolors=$(tput colors)
-    fi
-    if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
-        RED="$(tput setaf 1)"
-        GREEN="$(tput setaf 2)"
-        YELLOW="$(tput setaf 3)"
-        BOLD="$(tput bold)"
-        NORMAL="$(tput sgr0)"
-    else
-        RED=""
-        GREEN=""
-        YELLOW=""
-        BOLD=""
-        NORMAL=""
-    fi
 
     need git
     need zsh
@@ -40,35 +43,69 @@ main() {
 
     ensure umask g-w,o-w
 
-    printf "${BOLD}${YELLOW}Installing Oh My Zsh...${NORMAL}\n"
-    ensure mkdir -p "$ZSH"
-    ensure git clone --depth=1 "$OH_MY_ZSH_REPO" "$ZSH"
+    info "Installing Oh My Zsh..."
 
-    printf "installing zsh config...\n"
-    ensure mkdir -p "$ZDOTDIR"
-    ensure git clone "$ZSH_CONFIG_REPO" "$ZDOTDIR"
+    if git_repo "$ZSH"; then
+        warn "Already a git repository: '$ZSH'"
+    else
+        ensure mkdir -p "$ZSH"
+        ensure git clone --depth=1 "$OH_MY_ZSH_REPO" "$ZSH"
+    fi
+
+    info "installing zsh config..."
+
+
+    if git_repo "$ZDOTDIR"; then
+        warn "Already a git repository: '$ZDOTDIR'"
+    else
+        ensure mkdir -p "$ZDOTDIR"
+        ensure git clone "$ZSH_CONFIG_REPO" "$ZDOTDIR"
+    fi
 
     # ensure ZSH cache dir exists
     [ -d $ZSH_CACHE_DIR ] || mkdir -p $ZSH_CACHE_DIR
     # set update marker
     touch $ZSH_CACHE_DIR/last-update
 
-    printf "creating symlink from '$HOME/.zshenv' to '$ZDOTDIR/.zshenv'...\n"
+    info "Creating symlink from '$HOME/.zshenv' to '$ZDOTDIR/.zshenv'..."
     ensure ln -fs $(readlink -e "$ZDOTDIR/.zshenv") "$HOME/.zshenv"
 
-    printf 'changing your default shell to zsh...\n'
-    me=$USER
-    ensure as_root chsh -s $(grep "zsh$" /etc/shells | tail -1) "$me"
+    if [ "$(shell)" != "zsh" ]; then
+        info 'Changing your default shell to zsh...'
+        ensure as_root chsh -s $(grep "zsh$" /etc/shells | tail -1) "$USER"
+    fi
 
-    printf "${BOLD}${GREEN}"
-    printf 'ZSH config is successefully installed!\n'
-    printf "Type 'env zsh' to login to ZSH immediately\n"
-    printf "${NORMAL}"
+    success "ZSH config is successefully installed!"
+    success "Type 'env zsh' to login to ZSH immediately"
+}
+
+shell() {
+    echo $SHELL | awk -F/ '{ print $NF }'
+}
+
+git_repo() {
+    [ -d "$1/.git" ]
 }
 
 die() {
-    printf "${BOLD}${RED}ERROR${NORMAL}: %s\n" $1 >&2
+    printf "$(color "$BOLD$RED" "%s")\n" "$1" >&2
     exit 1
+}
+
+warn() {
+    printf "$(color "$BOLD$YELLOW" "%s")\n" "$1"
+}
+
+info() {
+    printf "$(color "$BOLD$BLUE" "%s")\n" "$1"
+}
+
+success() {
+    printf "$(color "$BOLD$GREEN" "%s")\n" "$1"
+}
+
+color() {
+    echo "$1$2$NORMAL"
 }
 
 need() {
