078836066c0a2c91260b6799da2925f505ec4eb2
smaximov <s.b.maximov@gmail.com>
2016-05-25 02:10:02 +1000

    Disable globs for some commands

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -1,10 +1,16 @@
-alias bower="noglob bower"
 alias e="nocorrect emacsclient -t"
 alias rm="rm -v"
 alias mv="mv -v"
 alias cp="cp -v"
 alias kill-emacs="emacsclient -t -e '(kill-emacs)'"
 alias sdcv="sdcv --color"
+
+# Disable globs for following commands
+alias find='noglob find'
+alias bower="noglob bower"
+alias apt-get='noglob apt-get'
+alias aptitude='noglob aptitude'
+alias yum='noglob yum'
 
 # Global aliases
 alias -g L='| less'
