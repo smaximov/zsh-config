23120e6c434b0838fd3ee3e1584d3cf0c40315bd
smaximov <s.b.maximov@gmail.com>
2016-05-29 20:37:14 +1000

    Remove `kill-emacs` alias

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -2,7 +2,6 @@ alias e="nocorrect emacsclient -t"
 alias rm="rm -v"
 alias mv="mv -v"
 alias cp="cp -v"
-alias kill-emacs="emacsclient -t -e '(kill-emacs)'"
 alias sdcv="sdcv --color"
 
 # Disable globs for following commands
