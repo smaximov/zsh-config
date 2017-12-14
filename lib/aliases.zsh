2aa35d829fbd78c26b7bf5b4b2692afc9a9255d1
smaximov <s.b.maximov@gmail.com>
2017-12-14 16:13:05 +0300

    Shorten exa alias

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -3,7 +3,7 @@ alias rm="rm -v"
 alias mv="mv -v"
 alias cp="cp -v"
 alias sdcv="sdcv --color"
-alias exl="exa -l"
+alias l="exa -l"
 
 # Disable globs for following commands
 alias find='noglob find'
